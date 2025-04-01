29d5261a76caed2349427d026572d846f4410992
Melih Dincer <m_dincer41@hotmail.com>
2025-04-01 22:27:47 +0300

    1 Nisan last commit.

--- src/main.jsx
+++ src/main.jsx
@@ -9,55 +9,88 @@ function ProductList() {
       "image":"1.png",
       "title":"IPhone 16",
       "description":"Lorem ipsum dolor sit amet.",
-      "price": 58000
+      "price": 58000,
+      "isActive": true
     },
     {
       "id":2,
       "image":"2.png",
       "title":"IPhone 16 Pro",
       "description":"Lorem ipsum dolor sit amet.",
-      "price": 88000
+      "price": 88000,
+      "isActive": true
     },
     {
       "id":3,
       "image":"3.png",
       "title":"IPhone 16 Pro Max",
       "description":"Lorem ipsum dolor sit amet.",
-      "price": 120000
+      "price": 120000,
+      "isActive": false
     }
   ]
 
   return (
     <>
     <h2>Product List</h2>
-    <div>
+    {items.length > 0 ? (
+    <div id='product-list'>
       {items.map((product, index) => (
         <Product key={index} 
         productObj = {product}
         />
-      ))
+      )) 
     
       }
-    </div>
-    
+    </div> ) : 
+    <p>Hiç ürün yok.</p>
+}
     </>
   ) 
 }
 
-function Product(props) {
-  console.log(props);
+function Product({productObj}) {
+  if (!productObj.isActive) return null;
   return(
     <div>
-      <img src={"/img/" + props.productObj.image} />
-      <h2>{props.productObj.title}</h2>
-      <p>{props.productObj.description}</p>
-      <span>{props.productObj.price} ₺</span>
+      <img src={"/img/" + productObj.image} />
+      <h2>{productObj.title}</h2>
+      <p>{productObj.description}</p>
+      <span>{productObj.price} ₺</span>
     </div>
+  )
+}
+
+function Header() {
+  return <h1>Header</h1>
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 8;
+  const closeHour = 20;
+
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen)
+
+  return(
+    <footer>
+      {
+        isOpen ? (
+          <p>Akşam {closeHour}'e kadar sipariş verebilirsiniz.</p>
+        ) : 
+        (
+          <p>Şuan kapalıyız. Açılış saatimiz: {openHour}</p>
+        ) 
+      }
+    </footer>
   )
 }
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Header />
     <ProductList />
+    <Footer />
   </StrictMode>,
 )
