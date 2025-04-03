8906fd50aec6c6fb0c3e223d62f3427fb0382a68
Melih Dincer <m_dincer41@hotmail.com>
2025-04-03 21:14:09 +0300

    3 Nisan last commit. -workshop sonu.

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "./main.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 function ProductList() {
   const items = [
     {
       "id":1,
-      "image":"1.png",
+      "image":"1.jpg",
       "title":"IPhone 16",
       "description":"Lorem ipsum dolor sit amet.",
       "price": 58000,
@@ -14,7 +17,7 @@ function ProductList() {
     },
     {
       "id":2,
-      "image":"2.png",
+      "image":"2.jpg",
       "title":"IPhone 16 Pro",
       "description":"Lorem ipsum dolor sit amet.",
       "price": 88000,
@@ -22,19 +25,59 @@ function ProductList() {
     },
     {
       "id":3,
-      "image":"3.png",
+      "image":"3.jpg",
       "title":"IPhone 16 Pro Max",
       "description":"Lorem ipsum dolor sit amet.",
       "price": 120000,
-      "isActive": false
+      "isActive": true
+    },
+    {
+      "id":3,
+      "image":"3.jpg",
+      "title":"IPhone 16 Pro Max",
+      "description":"Lorem ipsum dolor sit amet.",
+      "price": 120000,
+      "isActive": true
+    },
+    {
+      "id":3,
+      "image":"3.jpg",
+      "title":"IPhone 16 Pro Max",
+      "description":"Lorem ipsum dolor sit amet.",
+      "price": 120000,
+      "isActive": true
+    },
+    {
+      "id":3,
+      "image":"3.jpg",
+      "title":"IPhone 16 Pro Max",
+      "description":"Lorem ipsum dolor sit amet.",
+      "price": 120000,
+      "isActive": true
+    },
+    {
+      "id":3,
+      "image":"3.jpg",
+      "title":"IPhone 16 Pro Max",
+      "description":"Lorem ipsum dolor sit amet.",
+      "price": 120000,
+      "isActive": true
+    },
+    {
+      "id":8,
+      "image":"3.jpg",
+      "title":"IPhone 16 Pro Max",
+      "description":"Lorem ipsum dolor sit amet.",
+      "price": 120000,
+      "isActive": true
     }
   ]
 
   return (
     <>
-    <h2>Product List</h2>
+    <h2 className='title'>Product List</h2>
     {items.length > 0 ? (
-    <div id='product-list'>
+    <div id='product-list' className='row row-cols-2 row-cols-md-3 row-cols-xl-4 gap-4'>
       {items.map((product, index) => (
         <Product key={index} 
         productObj = {product}
@@ -52,17 +95,28 @@ function ProductList() {
 function Product({productObj}) {
   if (!productObj.isActive) return null;
   return(
-    <div>
-      <img src={"/img/" + productObj.image} />
+    <div className='card shadow-small'>
+      <img className='card-img-top p-2 p-md-3 border-bottom' src={"/img/" + productObj.image} />
+      <div className='card-body'>
       <h2>{productObj.title}</h2>
       <p>{productObj.description}</p>
-      <span>{productObj.price} ₺</span>
+      <span className={`badge text-bg-${productObj.price > 60000 ? "primary" : "success"}`}>{productObj.price} ₺</span>
+      </div>
     </div>
   )
 }
 
 function Header() {
-  return <h1>Header</h1>
+  
+  return (
+    <header>
+      <nav className='navbar navbar-expand bg-dark border-bottom border-body' data-bs-theme="dark">
+      <div className='container'>
+        <a href='#' className='navbar-brand'>Store App</a>
+      </div>
+      </nav>
+    </header>
+  )
 }
 
 function Footer() {
@@ -90,7 +144,8 @@ function Footer() {
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
+    <div className='container mt-3'>
     <ProductList />
-    <Footer />
+    </div>
   </StrictMode>,
 )
